c66250007d57f6c858805ae96b731716ed313aec
Victoria Cardoso <vvjcerque@hotmail.com>
2021-06-27 19:52:54 -0300

    Configurei o repositório

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-document.querySelector('body').style.background = 'red';
+class App{
+  //Construtor
+  construtor(){
+    //Lista de repositórios
+    this.repositorios = [];
+
+    //form
+    this.formulario = document.querySelector('form');
+
+    //método para registrar os eventos do form
+    this.registrarEventos();
+  }
+  
+  registrarEventos(){
+    this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
+  }
+
+  adicionarRepositorio(evento){
+    //evita que o formulario recarregue a página
+    evento.preventDefault();
+
+    //adiciona o repositório na Lista
+    this.repositorios.push({
+      nome: 'inventei',
+      descricao: 'sei la',
+      avatar_url: 'https://seila.com',
+      link: 'https://seila.com',
+    });
+  }
+}
+
+new App();
