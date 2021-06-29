35ec7904c1cbbc102f14383b0ac250339376fbd1
Victoria Cardoso <vvjcerque@hotmail.com>
2021-06-29 20:04:12 -0300

    Carregamento e erros

--- src/main.js
+++ src/main.js
@@ -1,100 +1,132 @@
 import api from './api';
 
 class App{
-  //Construtor
-  construtor(){
-    //Lista de repositórios
-    this.repositorios = [];
 
-    //form
-    this.formulario = document.querySelector('form');
+    //Construtor
+    constructor(){
+        // Lista de repositórios
+        this.repositorios = [];
 
-    //lista 
-    this.lista = document.querySelector('.list-group');
+        // Form
+        this.formulario = document.querySelector('form');
 
-    //método para registrar os eventos do form
-    this.registrarEventos();
-  }
-  
-  registrarEventos(){
-    this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
-  }
+        // Lista
+        this.lista = document.querySelector('.list-group');
 
-  async adicionarRepositorio(evento){
-    //evita que o formulario recarregue a página
-    evento.preventDefault();
-
-    //recupera o valor do input
-    let input = this.formulario.querySelector('input[id=repositorio]').value;
-
-    //se o input vier vazio... sai do app
-    if(input.length === 0){
-      return; //return sempre sai da função
+        // Método para registar os eventos do form
+        this.registarEventos();
     }
 
-    let response = await api.get(`/repos/${input}`);
-    
-    let {name, description, html_url, owner: {avatar_url}} = response.data;
+    registarEventos(){
+        this.formulario.onsubmit = evento => this.adicionarRepositorio(evento);
+    }
 
-    //adiciona o repositório na Lista
-    this.repositorios.push({
-      nome: name,
-      descricao: description,
-      avatar_url,
-      link: html_url,
-    });
+    async adicionarRepositorio(evento){
+        // Evita que o formulátio recarregue a página
+        evento.preventDefault();
 
-    //renderiza a tela
-    this.renderizarTela();
-  }
+        // Recuperar o valor do input
+        let input = this.formulario.querySelector('input[id=repositorio]').value;
 
-  renderizarTela(){
-    //limpa o conteúdo de lista
-    this.lista.innerHTML = '';
+        // Se o input vier vazio...sai da appp
+        if(input.length === 0){
+            return; //return sempre sai da função
+        }
 
-    //percorre toda a lista de repositório e cria os elementos
-    this.repositorios.forEach(repositorio =>{
-      
+        // Ativa o carregamento
+        this.apresentarBuscando();
 
-      //<li>
-      let li = document.createElement('li');
-      li.setAttribute('class', 'list-group-item list-group-item-action');
-    
-      //<img>
-      let img = document.createElement('img');
-      img.setAttribute('src', repositorio.avatar_url);
-      li.appendChild(img);
+        try{
+            let response = await api.get(`/repos/${input}`);
 
-      //<strong>
-      let strong = document.createElement('strong');
-      let txtNome = document.createTextNode(repositorio.nome);
-      strong.appendChild(txtNome);
-      li.appendChild(strong);
+            //console.log(response);
 
-      //<p>
-      let p = document.createElement('p');
-      let txtDescricao = document.createTextNode(repositorio.descricao);
-      p.appendChild(txtDescricao);
-      li.appendChild(p);
+            let { name, description, html_url, owner: { avatar_url } } = response.data;
 
-      //<a>
-      let a = document.createElement('a');
-      a.setAttribute('target', '_blank');
-      a.setAttribute('href', repositorio.link);
-      let txtA = document.createTextNode('Acessar');
-      a.appendChild(txtA);
-      li.appendChild(a);
+            // Adiciona o repositorio na lista
+            this.repositorios.push({
+                nome: name,
+                descricao: description,
+                avatar_url,
+                link: html_url,
+            });
 
-      // Adicionar <li> como filho da ul
-      this.lista.appendChild(li);
+            // Renderizar a tela
+            this.renderizarTela();
+        }catch(erro){
+            // Limpa buscando
+            this.lista.removeChild(document.querySelector('.list-group-item-warning'));
 
-      // Limpar o conteúdo do input
-      this.formulario.querySelector('input[id=repositorio]').value = '';
+            // Limpar erro existente
+            let er = this.lista.querySelector('.list-group-item-danger');
+            if(er !== null){
+                this.lista.removeChild(er);
+            }
 
-      // Adiciona o foco no input
-      this.formulario.querySelector('input[id=repositorio]').focus();
-    });
-  }
+            //<li>
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-danger');
+            let txtErro = document.createTextNode(`O repositório ${input} não existe.`);
+            li.appendChild(txtErro);
+            this.lista.appendChild(li);
+        }
+    }
+
+    apresentarBuscando(){
+        //<li>
+        let li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item list-group-item-warning');
+        let txtBuscando = document.createTextNode(`Aguarde, buscando o repositório...`);
+        li.appendChild(txtBuscando);
+        this.lista.appendChild(li);
+    }
+
+    renderizarTela(){
+        //Limpar o conteúdo de lista
+        this.lista.innerHTML = '';
+
+        // Percorrer toda a lista de repositórios e criar os elementos
+        this.repositorios.forEach(repositorio => {
+
+            //<li>
+            let li = document.createElement('li');
+            li.setAttribute('class', 'list-group-item list-group-item-action');
+
+            //<img>
+            let img = document.createElement('img');
+            img.setAttribute('src', repositorio.avatar_url);
+            li.appendChild(img);
+
+            //<strong>
+            let strong = document.createElement('strong');
+            let txtNome = document.createTextNode(repositorio.nome);
+            strong.appendChild(txtNome);
+            li.appendChild(strong);
+
+            //<p>
+            let p = document.createElement('p');
+            let txtDescricao = document.createTextNode(repositorio.descricao);
+            p.appendChild(txtDescricao);
+            li.appendChild(p);
+
+            //<a>
+            let a = document.createElement('a');
+            a.setAttribute('target', '_blank');
+            a.setAttribute('href', repositorio.link);
+            let txtA = document.createTextNode('Acessar');
+            a.appendChild(txtA);
+            li.appendChild(a);
+
+            // Adicionar <li> como filho da ul
+            this.lista.appendChild(li);
+
+            // Limpar o conteúdo do input
+            this.formulario.querySelector('input[id=repositorio]').value = '';
+
+            // Adiciona o foco no input
+            this.formulario.querySelector('input[id=repositorio]').focus();
+        });
+    }
 }
 
 new App();
